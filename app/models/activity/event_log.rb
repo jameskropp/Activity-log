b406b7704f88c81e070fa86da38590df63f32680
James Kropp <james-kropp@hotmail.com>
2018-06-27 17:09:18 -0700

    Add comment on event_log

--- app/models/activity/event_log.rb
+++ app/models/activity/event_log.rb
@@ -2,6 +2,7 @@ module Activity
   class EventLog < ApplicationRecord
     self.table_name = "activity_logs"
 
+    # Supported field names and scopes for each field
     SUPPORTED_FILTERS = [:user_id, :attached_id, :model, :event, :created_at, :updated_at]
     scope :user_id, -> (value) { where(user_id: value) }
     scope :attached_id, -> (value) { where(attached_id: value) }
@@ -10,6 +11,16 @@ module Activity
     scope :created_at, -> (value) { where(created_at: value) }
     scope :updated_at, -> (value) { where(updated_at: value) }
 
+    # Filter that finds all records that match a given request
+    # Example Request: attributes = {:attached_id: 1, event: "created_user"}
+    # - Will return any records where attached_id = 1 and event = "created_user"
+    # - Allows for simple searching with many unique cases
+    def self.filter(attributes)
+      attributes.slice(*SUPPORTED_FILTERS).reduce(all) do |scope, (key, value)|
+        value.present? ? scope.send(key, value) : scope
+      end
+    end
+
     def self.create_log(log)
       self.create!(
         user_id: log[:user_id],
@@ -17,12 +28,6 @@ module Activity
         model: log[:model],
         event: log[:event],
       )
-    end
-
-    def self.filter(attributes)
-      attributes.slice(*SUPPORTED_FILTERS).reduce(all) do |scope, (key, value)|
-        value.present? ? scope.send(key, value) : scope
-      end
     end
   end
 end
